1e5782e9f0b1600d0ca7d4682d92d9519d10c556
podee aupavan <100249@glo.or.th>
2025-11-20 14:34:00 +0700

    edit host for test

--- app/blog/feature/page.js
+++ app/blog/feature/page.js
@@ -3,7 +3,7 @@ export default async function Feature() {
   // const host = headerList.get("host");
   // console.log("Host:", host);
 
-  const data = await fetch("http://localhost:3000/api/blog");
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await data.json();
 
   return (
